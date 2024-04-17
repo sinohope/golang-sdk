f27e35e60a1082d41bd8ce093a1c21056a838c72
slowdickjoy1 <slowdickjoy@gmail.com>
2024-04-17 17:19:47 +0800

    brc20

---
+++ core/sdk/brc20_api_test.go
@@ -0,0 +1,140 @@
+package sdk
+
+import (
+	"encoding/json"
+	"github.com/sinohope/sinohope-golang-sdk/common"
+	"testing"
+)
+
+func TestInscribeDeploy(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.InscribeDeployParam{
+		RequestId:    "1",
+		Ticker:       "joy1",
+		TotalSupply:  "10000",
+		LimitPerMint: "100",
+		Decimals:     "6",
+		ChainSymbol:  "BTC_TEST",
+		From:         "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+	}
+	if err := c.InscribeDeploy(p); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInscribeMint(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.InscribeMintParam{
+		RequestId:   "2",
+		Ticker:      "joy1",
+		Amount:      "100",
+		ChainSymbol: "BTC_TEST",
+		From:        "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+	}
+	if err := c.InscribeMint(p); err != nil {
+		t.Error(err)
+	}
+}
+func TestInscribeTransfer(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.InscribeTransferParam{
+		RequestId:   "3",
+		Ticker:      "joy1",
+		Amount:      "10",
+		ChainSymbol: "BTC_TEST",
+		From:        "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+	}
+	if err := c.InscribeTransfer(p); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInscribeTransferById(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.TransferByIdParam{
+		RequestId:     "4",
+		ChainSymbol:   "BTC_TEST",
+		From:          "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+		To:            "2N9Fi5qKJTQDtq8CTGcqAFHRpyGDW5isbc1",
+		InscriptionId: "762972c705b604b64a6665faf5ef84ca0a4e3ebb46ba8ad495b8fb569d79187ei0",
+		Ticker:        "joy1",
+	}
+	if err := c.InscribeTransferById(p); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestOneStopTransfer(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.OneStopTransferParam{
+		RequestId:   "5",
+		Ticker:      "joy1",
+		Amount:      "10",
+		ChainSymbol: "BTC_TEST",
+		From:        "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+		To:          "2N9Fi5qKJTQDtq8CTGcqAFHRpyGDW5isbc1",
+	}
+	if err := c.OneStopTransfer(p); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestQueryInscribeTransfers(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.WaasBrc20QueryInscribeTransferReq{
+		Ticker:      "joy1",
+		ChainSymbol: "BTC_TEST",
+		Address:     "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+	}
+	if res, err := c.QueryInscribeTransfers(p); err != nil {
+		t.Error(err)
+	} else {
+		b, _ := json.Marshal(res)
+		t.Log(string(b))
+	}
+}
+
+func TestQueryPageBalanceSummary(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.WaasBrc20PageQueryBalanceSummaryReq{
+		ChainSymbol: "BTC_TEST",
+		Address:     "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+		Start:       0,
+		Limit:       10,
+	}
+	if res, err := c.QueryPageBalanceSummary(p); err != nil {
+		t.Error(err)
+	} else {
+		b, _ := json.Marshal(res)
+		t.Log(string(b))
+	}
+}
+
+func TestAddressTickerInfo(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.WaasBrc20QueryAddressTickerInfoReq{
+		Ticker:      "joy1",
+		ChainSymbol: "BTC_TEST",
+		Address:     "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+	}
+	if res, err := c.AddressTickerInfo(p); err != nil {
+		t.Error(err)
+	} else {
+		b, _ := json.Marshal(res)
+		t.Log(string(b))
+	}
+}
+
+func TestQueryAddressBalance(t *testing.T) {
+	c, _ := NewBrc20API(common.BaseUrl, common.FakePrivateKey)
+	p := &common.WaasBrc20QueryAddressBalanceReq{
+		ChainSymbol: "BTC_TEST",
+		Address:     "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+	}
+	if res, err := c.AddressBalance(p); err != nil {
+		t.Error(err)
+	} else {
+		b, _ := json.Marshal(res)
+		t.Log(string(b))
+	}
+}
--- core/sdk/transaction_api_test.go
+++ core/sdk/transaction_api_test.go
@@ -31,3 +31,85 @@ func TestFee(t *testing.T) {
 		t.Logf("%v", fee)
 	}
 }
+
+// encoding 3 -> 2
+func TestNested2NativeTransfer(t *testing.T) {
+	api, err := NewTransactionAPI(common.BaseUrl, common.FakePrivateKey)
+	if err != nil {
+		t.Fatalf("create new mpc api failed, %v", err)
+	}
+	param := &common.WalletTransactionSendWAASParam{
+
+		RequestId:   "100005",
+		ChainSymbol: "BTC_TEST",
+		AssetId:     "BTC_BTC_TEST",
+		From:        "2N9Fi5qKJTQDtq8CTGcqAFHRpyGDW5isbc1",
+		To:          "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+		Amount:      "100000",
+		VaultId:     "534605276521221",
+		WalletId:    "534606724211461",
+		ToTag:       "32143",
+		Note:        "用户交易信息备注",
+		Fee:         "50000",
+	}
+	res, err := api.CreateTransfer(param)
+	if err != nil {
+		t.Fatalf(err.Error())
+	} else {
+		t.Log(res)
+	}
+}
+
+// encode 2 -> 4
+func TestNative2Taproot(t *testing.T) {
+	api, err := NewTransactionAPI(common.BaseUrl, common.FakePrivateKey)
+	if err != nil {
+		t.Fatalf("create new mpc api failed, %v", err)
+	}
+	param := &common.WalletTransactionSendWAASParam{
+
+		RequestId:   "100006",
+		ChainSymbol: "BTC_TEST",
+		AssetId:     "BTC_BTC_TEST",
+		From:        "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+		To:          "tb1pv443quwd6n5mssmauukf0t37r3muxc0hmy45092n6yjxt2afd2hqqy8rze",
+		Amount:      "90000",
+		VaultId:     "534605276521221",
+		WalletId:    "534606724211461",
+		ToTag:       "32143",
+		Note:        "用户交易信息备注",
+		Fee:         "10000",
+	}
+	res, err := api.CreateTransfer(param)
+	if err != nil {
+		t.Fatalf(err.Error())
+	} else {
+		t.Log(res)
+	}
+}
+
+func TestLegacy2Native(t *testing.T) {
+	api, err := NewTransactionAPI(common.BaseUrl, common.FakePrivateKey)
+	if err != nil {
+		t.Fatalf("create new mpc api failed, %v", err)
+	}
+	param := &common.WalletTransactionSendWAASParam{
+
+		RequestId:   "100007",
+		ChainSymbol: "BTC_TEST",
+		AssetId:     "BTC_BTC_TEST",
+		From:        "n1QwsxKD3uZH9mmoCQKdsMbqi81tuD9QR3",
+		To:          "tb1q7a0jm6rlv8umckjjxqqnwdpze30w4rwafjj4hj",
+		Amount:      "90000",
+		VaultId:     "534605276521221",
+		WalletId:    "534606724211461",
+		ToTag:       "32143",
+		Note:        "用户交易信息备注",
+	}
+	res, err := api.CreateTransfer(param)
+	if err != nil {
+		t.Fatalf(err.Error())
+	} else {
+		t.Log(res)
+	}
+}
