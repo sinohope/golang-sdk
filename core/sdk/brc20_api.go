c7c0b753791941a5fe65b61d418f1123d618bae6
slowdickjoy1 <slowdickjoy@gmail.com>
2024-04-19 20:42:53 +0800

    brc20 modify comment

--- core/sdk/brc20_api.go
+++ core/sdk/brc20_api.go
@@ -22,7 +22,7 @@ func NewBrc20API(baseUrl, private string) (features.Brc20Api, error) {
 	}, nil
 }
 
-// InscribeDeploy 铭刻deploy铭文
+// InscribeDeploy Inscribe deploy inscription
 // POST: /v1/waas/mpc/brc20/inscribe_deploy
 func (t *brc20Api) InscribeDeploy(param *commonData.InscribeDeployParam) error {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/inscribe_deploy", param); err != nil {
@@ -35,7 +35,7 @@ func (t *brc20Api) InscribeDeploy(param *commonData.InscribeDeployParam) error {
 	}
 }
 
-// InscribeMint 铭刻mint铭文
+// InscribeMint Inscribe mint inscription
 func (t *brc20Api) InscribeMint(param *commonData.InscribeMintParam) error {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/inscribe_mint", param); err != nil {
 		return fmt.Errorf("send request failed, %v", err)
@@ -47,7 +47,7 @@ func (t *brc20Api) InscribeMint(param *commonData.InscribeMintParam) error {
 	}
 }
 
-// InscribeTransfer 铭刻transfer铭文
+// InscribeTransfer Inscribe transfer inscription
 func (t *brc20Api) InscribeTransfer(param *commonData.InscribeTransferParam) error {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/inscribe_transfer", param); err != nil {
 		return fmt.Errorf("send request failed, %v", err)
@@ -59,7 +59,7 @@ func (t *brc20Api) InscribeTransfer(param *commonData.InscribeTransferParam) err
 	}
 }
 
-// InscribeTransferById Brc20铭文转账
+// InscribeTransferById Brc20 inscription transfer
 func (t *brc20Api) InscribeTransferById(param *commonData.TransferByIdParam) error {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/inscribe_transfer_by_id", param); err != nil {
 		return fmt.Errorf("send request failed, %v", err)
@@ -71,7 +71,7 @@ func (t *brc20Api) InscribeTransferById(param *commonData.TransferByIdParam) err
 	}
 }
 
-// OneStopTransfer 一站式brc20转账
+// OneStopTransfer One-stop brc20 transfer
 func (t *brc20Api) OneStopTransfer(param *commonData.OneStopTransferParam) error {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/one_stop_transfer", param); err != nil {
 		return fmt.Errorf("send request failed, %v", err)
@@ -83,7 +83,7 @@ func (t *brc20Api) OneStopTransfer(param *commonData.OneStopTransferParam) error
 	}
 }
 
-// QueryInscribeTransfers 查询指定地址的指定资产的BRC20可转铭文信息
+// QueryInscribeTransfers Query BRC20 transferable inscription information of specified assets at specified address
 func (t *brc20Api) QueryInscribeTransfers(param *commonData.WaasBrc20QueryInscribeTransferReq) (*commonData.WaasBrc20QueryInscribeTransfersRes, error) {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/query_inscribe_transfers", param); err != nil {
 		return nil, fmt.Errorf("send request failed, %v", err)
@@ -99,7 +99,7 @@ func (t *brc20Api) QueryInscribeTransfers(param *commonData.WaasBrc20QueryInscri
 	}
 }
 
-// QueryPageBalanceSummary 指定地址的所有BRC20 资产余额信息 （分页查询）
+// QueryPageBalanceSummary All BRC20 asset balance information for the specified address (paged query)
 func (t *brc20Api) QueryPageBalanceSummary(param *commonData.WaasBrc20PageQueryBalanceSummaryReq) (*commonData.WaasBrc20PageQueryBalanceSummaryRes, error) {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/page_balance_summary", param); err != nil {
 		return nil, fmt.Errorf("send request failed, %v", err)
@@ -130,7 +130,7 @@ func (t *brc20Api) AddressTickerInfo(param *commonData.WaasBrc20QueryAddressTick
 	}
 }
 
-// AddressBalance 查询指定地址BTC余额详情（总余额、非铭文余额、铭文相关余额）
+// AddressBalance Query the details of the BTC balance of the specified address (total balance, non-inscription balance, inscription related balance)
 func (t *brc20Api) AddressBalance(param *commonData.WaasBrc20QueryAddressBalanceReq) (*commonData.WaasBrc20QueryAddressBalanceRes, error) {
 	if response, err := t.gw.Post("/v1/waas/mpc/brc20/address_balance", param); err != nil {
 		return nil, fmt.Errorf("send request failed, %v", err)
